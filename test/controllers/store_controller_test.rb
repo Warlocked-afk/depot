d74c7e1296913598239fe44e0606e379620f2b71
Warlocked-afk <scottgreen1998@gmail.com>
2019-10-19 19:13:27 +0100

    Added a store index

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -4,6 +4,7 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get store_index_url
     assert_response :success
+    @products = Product.order(:title)
   end
 
 end
