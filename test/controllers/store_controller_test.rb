726b5c7a3de845e48884aab4b700727b8ad96b26
Warlocked-afk <scottgreen1998@gmail.com>
2019-10-21 10:31:58 +0100

    Added cart functionality

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -3,6 +3,10 @@ require 'test_helper'
 class StoreControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get store_index_url
+    assert_select 'nav.side_nav a', minimum: 4
+    assert_select 'main ul.catalog li', 3
+    assert_select 'h2', 'Programming Ruby 1.9'
+    assert_select '.price', /\$[,\d]+\.\d\d/
     assert_response :success
     @products = Product.order(:title)
   end
