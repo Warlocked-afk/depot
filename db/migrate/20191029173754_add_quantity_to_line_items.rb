f15b5bf3e5032e022636b03db4b7a39e04154377
Warlocked-afk <scottgreen1998@gmail.com>
2019-11-18 14:17:12 +0000

    include cart in store layout

--- db/migrate/20191029173754_add_quantity_to_line_items.rb
+++ db/migrate/20191029173754_add_quantity_to_line_items.rb
@@ -1,27 +1,5 @@
 class AddQuantityToLineItems < ActiveRecord::Migration[6.0]
-  def up
-    add_column :line_items, :quantity, :integer, default: 1
-    sums = cart.line_items.group(:product_id).sum(:quantity)
-    sums.each do |product_id, quantity|
-      if quantity > 1
-        # remove individual items
-        cart.line_items.where(product_id: product_id).delete_all
-        # replace with single item and record quantity
-        item = cart.line_items.build(product_id: product_id)
-        item.quantity = quantity
-        item.save!
-      end
-    end
-  end
-
-  def down
-    # split items with quantity>1 into multiple items
-    LineItem.where("quantity>1").each do |item|
-      # add individual items
-        item.quantity.times do
-        LineItem.create(cart_id: item.cart_id,product_id: item.product_id, quantity: 1)
-    end
-    # remove original item
-    item.destroy
-  end
+#  def change
+#    add_column :line_items, :quantity, :integer, default: 1
+#  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_30_172952) do
+ActiveRecord::Schema.define(version: 2019_10_29_173754) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
